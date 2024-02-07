150b2761125e7c9e3c9ffc86d67453d6b3a45a48
Jang2723 <jangsb46@gmail.com>
2024-02-07 15:53:42 +0900

    02/07 JpaRepository와 커스텀 구현체 연결 + Paging 해보기

--- src/main/java/com/example/querydsl/dto/ItemDto.java
+++ src/main/java/com/example/querydsl/dto/ItemDto.java
@@ -1,11 +1,11 @@
 package com.example.querydsl.dto;
 
+import com.example.querydsl.entity.Item;
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
-//@AllArgsConstructor
 @ToString
 public class ItemDto { // QuerydslProjTests의 Tuple을 Dto로 바꾸기 위한 실습
     private String name;
@@ -21,6 +21,13 @@ public class ItemDto { // QuerydslProjTests의 Tuple을 Dto로 바꾸기 위한 
         this.name = name;
         this.cost = cost;
         this.stock = stock;
-        this.totalCost = cost * stock;
+    }
+
+    public static ItemDto fromEntity(Item entity) {
+        return new ItemDto(
+                entity.getName(),
+                entity.getPrice(),
+                entity.getStock()
+        );
     }
 }
